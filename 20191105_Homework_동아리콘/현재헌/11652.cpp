4503fe413ccc880b4a360b2c4a8814dc5a84909b
HaswellAoba <42887520+HaswellAoba@users.noreply.github.com>
2019-11-07 17:06:49 +0900

    BOJ 11652 우와

---
+++ 20191105_Homework_동아리콘/현재헌/11652.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<algorithm>
+long long int storage[1000006];
+
+int main() {
+	int n;
+	int  i, index = 0, count = 0;
+	long long int hd=0;
+	scanf("%d", &n);
+	for (i = 0; i < n; i++) {
+		scanf("%lld", &storage[i]);
+	}
+	std::sort(storage, storage + n );
+
+	hd = storage[0];
+
+	for (i = 0; i < n; i++) {
+		if (storage[i] == storage[i + 1]) count++;
+		else count = 0;
+		if (index < count) {
+			index = count;
+			hd = storage[i];
+		}
+	}
+	
+	printf("%lld", hd);
+}
